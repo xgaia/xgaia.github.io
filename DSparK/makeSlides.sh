21461cd4d30c8ac3e0be0b1887d6aabb24c19382
xgaia <xgaia@gmx.com>
2018-04-11 16:06:38 +0200

    script watch md file, and rebuild html when it changes

--- DSparK/makeSlides.sh
+++ DSparK/makeSlides.sh
@@ -1,6 +1,54 @@
 #! /bin/bash
 
-markdown-to-slides README.md -o index.html
+# This script check every seconds id the markdon file is modified.
+# If yes, the html file is rebuilt
 
+
+mdfile="README.md"
+cssfile="style.css"
+templateile="template.html"
+htmlfile="index.html"
+
+# First, build html
+markdown-to-slides README.md  -s style.css -l template.html -o index.html
 # sed js link
 sed -i s@https\:\/\/gnab\.github\.io\/remark\/downloads@\.\.@g index.html
+
+# Get md5sum
+md5=$(md5sum $mdfile | cut -d " " -f1)
+
+
+
+while true ;do
+    # recompute md5
+    newmd5=$(md5sum $mdfile | cut -d " " -f1)
+
+    # echo "md5:    $md5"
+    # echo "newmd5: $newmd5"
+
+    if [[ "$newmd5" != "$md5" ]]; then
+        echo "README.md change, re-build html ..."
+        # build html
+        markdown-to-slides README.md  -s style.css -l template.html -o index.html
+        # sed js link
+        sed -i s@https\:\/\/gnab\.github\.io\/remark\/downloads@\.\.@g index.html
+        echo "Done"
+    fi
+
+    md5=$(md5sum $mdfile | cut -d " " -f1)
+    sleep 1s
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
